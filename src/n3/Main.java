e19e0a9e8883ea5f386276887e351c8999a7a49a
N3ther <thenetherreactor@gmail.com>
2021-08-10 01:35:54 -0400

    added test of capabilities
    wont actually do anything of use, right now it's more of a "find a random conlang" feature. Will probably be removed, but for now it'll work while I wait for an API to push to

--- src/n3/Main.java
+++ src/n3/Main.java
@@ -9,12 +9,19 @@ public class Main {
 		System.out.println("--BRCL GET--");
 		GetRequest.getBRCL();
 		System.out.println("--CLCR GET--");
-		String[][] CLCRArray = GetRequest.getCLCR();
-		System.out.println("94th entry's source: " + CLCRArray[93][0]);
-		System.out.println("94th entry's code: " + CLCRArray[93][1]);
-		System.out.println("94th entry's name: " + CLCRArray[93][3]);
-		System.out.println("94th entry's URL: " + CLCRArray[93][4]);
-		System.out.println("94th entry's author: " + CLCRArray[93][5]);
-		System.out.println("94th entry's date of creation: " + CLCRArray[93][6]);
+		String[][] CLCRArray = GetRequest.getCLCR(); //For second number, meaning is as follows: 
+		//0 - Source; 1 - Short Code; 2 - Long Code; 3 - Name; 4 - URL; 5 - Author; 6 - Date
+		
+		//the following is a test to show the capabilities of the GET request.
+		int random = (int)(Math.random()*236);
+		System.out.println((random + 1) + " entry's source: " + CLCRArray[random][0]);
+		System.out.println((random + 1) + " entry's code: " + CLCRArray[random][1]);
+		if (CLCRArray[random][2] != null && !CLCRArray[random][2].isEmpty()) {
+			System.out.println((random + 1) + " entry's long code: " + CLCRArray[random][2]);
+		}
+		System.out.println((random + 1) + " entry's name: " + CLCRArray[random][3]);
+		System.out.println((random + 1) + " entry's URL: " + CLCRArray[random][4]);
+		System.out.println((random + 1) + " entry's author: " + CLCRArray[random][5]);
+		System.out.println((random + 1) + " entry's date of creation: " + CLCRArray[random][6]);
 	}
 }
